ae8f15c5599780ff4ccf9b64c0bb6926f6c420fd
Gr4vity4 <prawared.bw@gmail.com>
2019-05-30 15:28:52 +0700

    Add check answer correct

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -4,7 +4,8 @@
             {{ `${id}. ${quiz.question}` }}
         </p>
         <div v-for="(answer, index) in answers" :key="index" class="text-left pl-8">
-            <input type="radio" class="mr-2" :name="`answer_${id}`">
+            <input type="radio" class="mr-2" :name="`answer_${id}`" :value="answer"
+                   @click="selected">
             {{ answer }}
         </div>
     </div>
@@ -17,6 +18,18 @@
       'id',
       'quiz',
     ],
+    data: function () {
+      return {
+        answerCorrect: this.quiz.correct_answer,
+      }
+    },
+    methods: {
+      selected: function (x) {
+        let answerSelected = x.target.value
+        console.log('you selected is : ' + answerSelected)
+        console.log('answer is : ' + this.answerCorrect)
+      },
+    },
     computed: {
       answers: function () {
         let data = [...this.quiz.incorrect_answers]
